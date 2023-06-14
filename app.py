760211350d3608fcd89e731e9381f7f073d716fc
AURELIANO BASSO <71911487+yano79@users.noreply.github.com>
2023-06-14 13:15:34 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,23 +1,48 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from models import ContactForm
+
 
 app = Flask(__name__)
+ckeditor = CKEditor(app)
+Bootstrap(app)
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = url_object
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 @app.route('/')
 def home():  # put application's code here
     return render_template("index.html")
 
-@app.route('/right')
-def right():
-    return  render_template("right-sidebar.html")
+@app.route('/documents')
+def documents():
+    return render_template("documents.html")
 
-@app.route('/left')
-def left():
-    return  render_template("left-sidebar.html")
+@app.route('/experience')
+def experience():
+    return render_template("experience.html")
 
-@app.route('/no_sidebar')
-def no_sidebar():
-    return  render_template("no-sidebar.html")
+@app.route('/references')
+def references():
+    return render_template("references.html")
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    new_form = ContactForm()
+    if new_form.validate_on_submit():
+        name = new_form.name.data
+        email = new_form.email.data
+        message = new_form.message.data
+        text = f"{name}\n{email}\n{message}"
+        new_form.email_notification(text=text)
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", form=new_form)
+
+@app.route('/gallery')
+def gallery():
+    return render_template("gallery.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,40 @@
+from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length, Email
+import smtplib
+from email.mime.text import MIMEText
+
+MY_EMAIL = "aureliano.basso@gmail.com "
+PASSWORD = "loggnutchvjfvzbg"
+
+
+class ContactForm(FlaskForm):
+    name = StringField(label='Full name:', validators=[DataRequired(), Length(max=50)])
+    email = EmailField(label='Email Address:', validators=[DataRequired(), Length(max=100), Email()])
+    phone = StringField(label='Phone Number: (optional)')
+    message = CKEditorField(label='Your message:', validators=[DataRequired(), Length(max=5000)])
+    submit = SubmitField('Send')
+
+    def email_notification(self, text: str):
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+            connection.connect("smtp.gmail.com", 465)
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            subject = "Potential employer enquiry !!!!!"
+            sender = "aureliano.basso@gmail.com"
+            recipients = (
+                "aureliano.basso@gmail.com"
+            )
+            Ccopies = ""
+            body = text
+            msg = MIMEText(body)
+            msg["Subject"] = subject
+            msg["From"] = sender
+            msg["To"] = recipients
+            msg["Cc"] = Ccopies
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=msg["To"].split(",") + msg["Cc"].split(","),
+                msg=f"Subject: {subject}\n\n" + body)
+
+
